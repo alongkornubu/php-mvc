629fca5fe5e53d8c2ee355208e3e8f5779eccb6a
alongkornubu <alongkorn.ch.62@ubu.ac.th>
2024-04-28 21:43:46 +0700

    first commit

---
+++ core/Router.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\core;
+
+class Router {
+    public function get($path,$callback) {
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+use app\core\Application;
+
+$app = new Application();
+// $router = new Router();
+
+$app->router->get('/', function() {
+    return 'Hello World';
+});
+
+$app->router->get('contact', function() {
+    return 'Contact';
+});
+
+
+
+$app->run();
